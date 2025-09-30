5d38c3230ccd0b652442e48d5ed4c8f9d7ff037c
Muskaan-Yousaf <you124@stge.org.uk>
2025-09-30 09:24:23 +0000

    aaaaaaaaaaaaaaaaaa

---
+++ Loops/ifStatements.py
@@ -0,0 +1,61 @@
+ 
+
+#Write a Python program that 
+#asks the user for their age and prints whether they
+#  are eligible to watch 18-rated movies. 
+# userAge = int(input("What's your age?" ))
+# if userAge >= 18:
+#     print("You are eligible to watch this 18+ movie.")
+# else:
+#     print("You are not eligible to watch this 18+ movie.")
+
+#Write a Python program that asks the user for their age and
+# prints whether they can watch films rated 18, 15, or below 15. 
+# userAge = int(input("What's your age?" ))
+# if userAge >= 18:
+#      print("You are eligible to watch movies at any rate.")
+# elif userAge >= 15:
+#      print("You are eligible to watch movies rated 15 or under.")
+# elif userAge < 15:
+#      print("You are eligible to watch under 15 movies only.")
+
+#Write a Python program that asks the user how they are feeling. 
+# Print different suggestions depending on their emotion. 
+# Include at least three different elif statements. 
+# userEmotion = str(input("How are you feeling?(Happy/Sad/Angry/Hungry): "))
+# if userEmotion == "Happy":
+#     print("That's great!")
+# elif userEmotion == "Sad":
+#     print("Aw, do you need a hug?")
+# elif userEmotion == "Angry":
+#     print("Take some deep breaths and time out.")
+# elif userEmotion == "Hungry":
+#     print("Go eat something.")
+
+# Write a Python program that asks the user what genre of film they enjoy. 
+# Print a list of recommendations depending on the genre. 
+# Include at least three different elif statements. 
+# userFaveGenre = str(input("What's your favourite genre? (Action/Horror/Romance/Comedy): "))
+# if userFaveGenre == "Action":
+#     print("Reccomendations: Terminator 2: Judgment Day, Die Hard or Mission: Impossible - Fallout")
+# elif userFaveGenre == "Horror":
+#     print("Reccomendations: IT, The Shining or Jaws")
+# elif userFaveGenre == "Romance":
+#     print("Reccomendations: Romeo and Juliet [1996], 10 Things I Hate About You or Titanic")
+# elif userFaveGenre == "Comedy":
+#     print("Reccomendations: Freaky Friday, Parent Trap or Mean Girls")
+
+# Write a Python program that asks the user to enter two numbers and then choose an operation (add, multiply, subtract, divide). 
+# Perform the chosen calculation and print the result.
+# Make sure you handle division by zero. 
+# userCalc1 = int(input("Enter one number: "))
+# userCalc2 = int(input("Enter another number: "))
+# userOperation = (input("Enter an operation (Addition/Subtraction/Division/Multiplication): "))
+# if userOperation == "Addition":
+#     print(userCalc1 + userCalc2)
+# elif userOperation == "Subtraction":
+#     print(userCalc1 - userCalc2)
+# elif userOperation == "Division":
+#     print(userCalc1 / userCalc2)
+# elif userOperation == "Multiplication":
+#     print(userCalc1 * userCalc2)
---
+++ Loops/logic.py
@@ -0,0 +1,9 @@
+#Write a Python program that assigns values to variables representing the temperature and whether it is sunny. 
+# Check if the temperature is greater than 15 and it is sunny. Print the result. 
+
+temp = int(input("Enter temperature: "))
+weather = str(input("Enter weather: "))
+if temp > 15 and weather == "Sunny":
+    print("It's 15 degrees and sunny!")
+
+#[UNFINISHED]
