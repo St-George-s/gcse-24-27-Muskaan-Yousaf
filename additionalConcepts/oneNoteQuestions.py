dd5d452250b6b2e282aefcaebe17c5da9ea6251f
Muskaan-Yousaf <you124@stge.org.uk>
2025-11-04 10:07:31 +0000

    DONE

---
+++ additionalConcepts/oneNoteQuestions.py
@@ -0,0 +1,37 @@
+# # Write a Python program to extract the first and last characters of a string entered by the user. 
+# string = input("gimme a string: ")
+# print(string[0])
+# print(string[-1])
+
+# Write a Python program to reverse a string entered by the user. 
+# userString = input("give me another string: ")
+# reverseString = ""
+# for character in userString:
+#     reverseString = character + reverseString
+# print(reverseString)
+
+# Write a Python program to check if a string is a palindrome (reads the same forward and backward). 
+
+
+# Write a Python program to count the number of vowels in a string. 
+
+# Write a Python program to replace all spaces in a string with underscores (_). 
+
+ 
+
+# Generate and print one random integer from 1 to 6. 
+# import random
+# myRandomNumber = random.randint(1, 6)
+# print(myRandomNumber)
+
+# Roll two dice (1–6) and print both values and their total using + string concatenation. 
+import random
+myRandomNumber = random.randint(1, 6)
+myRandom2 = random.randint(1, 6)
+print(str(myRandomNumber), " + ", str(myRandom2), " = ")
+print(str(myRandomNumber + myRandom2))
+
+
+# Given the list below, use a random integer to select a valid index and print the item at that index. 
+#animals = ["otter", "red squirrel", "pine marten", "seal", "oystercatcher"] 
+ 
---
+++ additionalConcepts/randomNumbers.py
@@ -0,0 +1,5 @@
+import random
+
+myRandomNumber = random.randint(1, 6)
+print(myRandomNumber)
+
