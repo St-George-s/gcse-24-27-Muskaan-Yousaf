8620f7647f9e6ebdaf66f6e23a095232b2cabd0c
Muskaan-Yousaf <you124@stge.org.uk>
2026-01-22 13:13:44 +0000

    WOOO DONE

--- codingChallengeJan26/codingChallengeA.py
+++ codingChallengeJan26/codingChallengeA.py
@@ -1,20 +1,43 @@
-import random 
+# import random 
 
+# counter = 0
+# ranNum = random.randint(1, 50)
+# userGuess = 0
+
+# while userGuess != ranNum:
+#     userGuess = int(input("Enter a number 1 to 50, try to guess the random number!: "))
+
+#     if userGuess < ranNum:
+#         print("Your guess is too low.")
+#     counter = counter + 1 
+
+# else:
+#     print("Your guess is too high.")
+#     counter = counter + 1
+
+#     if userGuess == ranNum:
+#         print("Well done, you got the correct answer!", "you tried", counter, "times!")
+# #NOT FINISHED VIEW FLOWCHART IN JOTTER FOR DETAILS
+
+import random
+ 
 counter = 0
 ranNum = random.randint(1, 50)
-userGuess = input(int("Enter a number 1 to 50, try to guess the random number! "))
+userGuess = 0
+ 
+while userGuess != ranNum:
+ 
+    userGuess = int(input("Enter a number 1 to 50, try to guess the random number!: "))
+ 
+    if userGuess < ranNum:
+        print("Your guess is too low.")
+        counter = counter + 1
+ 
+    else:
+        print("Your guess is too high.")
+        counter = counter + 1
+ 
+    if userGuess == ranNum:
+        print("Well done, you got the correct answer!", "you tried", counter, "times!")
 
-while userGuess!= ranNum:
-userGuess = input(int("Enter a number 1 to 50, try to guess the random number! "))
-
-if userGuess < ranNum:
-    print("Your guess is too low.")
-    counter = counter + 1 
-
-else:
-    print("Your guess is too high.")
-    counter = counter + 1
-
-if userGuess == ranNum:
-    print("Well done, you got the correct answer!", "you tried", counter, "times!")
-#NOT FINISHED VIEW FLOWCHART IN JOTTER FOR DETAILS
+    #FINISHED!
