5d0b47686cc76ed5bcaaed49de9d11dfe813b73a
Muskaan-Yousaf <you124@stge.org.uk>
2026-06-09 09:21:46 +0000

    rgsrddefewgetrrhrfgrg

---
+++ functionsAndProcedures.py/functionsAndProcedures.py
@@ -0,0 +1,35 @@
+# #procedure
+# def printName(name):
+#     print(name)
+
+# #Function
+# def printNameFunc(name):
+#     return name
+
+# #call the procedure
+# printName("Muskaan")
+
+# #call the function and print returned value
+# print(printNameFunc("Josh Dun"))
+
+# #call the function and store returned value in a variable
+# returnedName = printNameFunc("Tyler Joseph")
+# print(returnedName)
+
+#questions
+
+# def add_numbers(a, b):
+#     total = a + b
+#     return total
+# result = add_numbers(5, 3)
+# print(result)
+
+# def greet(name):
+#     print("Hello", name)
+
+# greet("COCA COLA HELLO")
+
+def sphere_volume(radius):
+    return = (4/3 * 3.14 * (radius) ** 3)
+
+#UNFINISHED
